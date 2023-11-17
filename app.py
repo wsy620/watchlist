9b138f0e957c01b4dc5660005b4d25d487f79af3
Wu Siying <1229536509@qq.com>
2023-11-17 18:54:13 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -37,11 +37,7 @@ def initdb(drop):
 
 
 
-@app.route('/')
-def index():
-    user = User.query.first() # 读取用户记录
-    movies = Movie.query.all() # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
+
 
 @app.cli.command()
 def forge():
@@ -69,3 +65,19 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
+
+
+
+@app.context_processor
+def inject_user(): # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user) # 需要返回字典，等同于return {'user': user}
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route('/')
+def index():
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
